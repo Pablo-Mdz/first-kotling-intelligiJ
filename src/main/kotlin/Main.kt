a2e1dfc888e0d6e1667c5e0124d7d0c452cb9455
Pablo-Mdz <pablocigoy@gmail.com>
2023-10-16 11:41:00 +0200

    first commit intelliiJ

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //Schreibe hier deinen Code
+
+    println("Ich werde Ihnen einige Fragen stellen ...")
+    println("Wie heiß du? (bitte confirm mit ENTER)")
+    var name = readln()
+    println("Willkomen $name!")
+    println("wie alt bist du? (bitte confirm mit ENTER)")
+    var alt = readln()
+    println("richtig? $alt?  du bis sehr alt $name... ")
+}
